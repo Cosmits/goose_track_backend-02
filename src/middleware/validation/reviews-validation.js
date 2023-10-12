d1a9efe9ba72099c8c4df4a28cb7ca3ee142d10b
Illia Shatokhin <resonane.2106@gmail.com>
2023-10-13 00:32:14 +0300

    add patch validate middleware

--- src/middleware/validation/reviews-validation.js
+++ src/middleware/validation/reviews-validation.js
@@ -2,7 +2,9 @@ import validateBody from "../../decorators/validateBody.js";
 import reviewsSchemas from "../../schemas/reviews-schemas.js";
 
 const addReviewValidate = validateBody(reviewsSchemas.reviewsSchema);
+const updateReviewValidate = validateBody(reviewsSchemas.reviewsSchema);
 
 export default {
   addReviewValidate,
+  updateReviewValidate,
 };
