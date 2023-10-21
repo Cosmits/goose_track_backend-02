524b3acc0201df9a7abf574805579dbec0ca75fb
Cosmits <salge.cos@gmail.com>
2023-10-21 15:56:11 +0200

    clear code

--- server.js
+++ server.js
@@ -2,15 +2,11 @@ import mongoose from "mongoose";
 import app from "./app.js";
 import normalizePort from "./src/helpers/normalizePort.js";
 
-// import "dotenv/config";
 import dotenv from "dotenv";
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
 const PORT = normalizePort(process.env.PORT || "5005");
 const DB_HOST = process.env.DB_HOST;
-
-// console.log("🚀 ~ file: server.js:8 ~ DB_HOST:", DB_HOST)
-// console.log("🚀 ~ file: server.js:9 ~ process.env.NODE_ENV:", `.env.${process.env.NODE_ENV}`)
 
 mongoose
   .connect(DB_HOST, {
