fe1c9257738798c0e42096551e3a1e2577b5e0f8
Cosmits <salge.cos@gmail.com>
2023-11-19 23:35:35 +0100

    add isValidUserId

--- src/middleware/validation/index.js
+++ src/middleware/validation/index.js
@@ -1,2 +1,5 @@
 export { default as isValidId } from "./isValidId.js";
 export { default as isValidTime } from "./isValidTime.js";
+export { default as isValidUserId } from "./isValidUserId.js";
+
+
